292b7e726f8c5eed949061502077381abff67c30
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-09-11 09:58:52 +0900

    feat: sum

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+	public static int sum(int n, int m) {
+		return n+m;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("장준하");
+		
+		System.out.println("당신은 무슨 학과 학생인가요?");
+		
+		System.out.println("저는 컴공과 학생입니다.");
+		
+		int a = 20;
+		
+		System.out.println(a);
+		
+		int s;
+		s = sum(a, 10);
+		System.out.println(s);
+	}
+}
