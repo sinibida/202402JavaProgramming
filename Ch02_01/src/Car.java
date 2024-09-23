7563a67d3c726422f816209c300f7186ee8b9efc
SPAUPA <sinibida@gmail.com>
2024-09-23 14:23:22 +0900

    +: circle

---
+++ Ch02_01/src/Car.java
@@ -0,0 +1,16 @@
+
+public class Car {
+
+	public static void main(String[] args) {
+		final double PI = 3.141592;
+		
+		double radius = 10.0;
+		
+		double area = radius * radius * PI;
+		double circum = 2 * radius * PI;
+		
+		System.out.println(area);
+		System.out.println(circum);
+	}
+
+}
