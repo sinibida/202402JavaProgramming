bcecccabdf5a843146d08aa3ef7ab8577a9449e7
SPAUPA <sinibida@gmail.com>
2024-09-23 14:09:50 +0900

    *: keywords

--- Ch02_01/src/Hello.java
+++ Ch02_01/src/Hello.java
@@ -1,31 +1,17 @@
 
 public class Hello {
-	public static double avg(int a, int b) {
-		return (double)sum(a, b) / 2.0;
-	}
-	
-	public static int sum(int n, int m) {
-		return n + m;
-	}
-	
-	// main() 메소드에서 실행 시작
 	public static void main(String[] args) {
-		int i = 20;
-		int s;
-		char a;
-		s = sum(i, 10); // sum() 메소드 호출
-		a = '?';
-		System.out.println(a); // 문자 '?' 화면 출력
-		System.out.println("Hello"); // "Hello" 문자열 화면 출력
-		System.out.println(s); // 정수 s 값 화면 출력
+		int myAge;
+		boolean isSingle;
 		
-		double as;
-		
-		as = avg(i, 10);
-		System.out.println(as);
-
-		System.out.print("Hello");
-		System.out.print("\n");
-		System.out.println("Hello");
+		final double PI = 3.141592;
 	}
+	
+	public int getAge() {
+		return 0;
+	}
+}
+
+class AutoVendingMachine {
+	
 }
