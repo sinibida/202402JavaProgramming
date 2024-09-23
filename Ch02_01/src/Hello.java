0756fd96c5caed07d5617370bb51caef857573a1
SPAUPA <sinibida@gmail.com>
2024-09-23 13:17:18 +0900

    +: test code

--- Ch02_01/src/Hello.java
+++ Ch02_01/src/Hello.java
@@ -6,8 +6,15 @@ public class Hello {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10); // sum() 메소드 호출
+		a = '?';
+		System.out.println(a); // 문자 '?' 화면 출력
+		System.out.println("Hello"); // "Hello" 문자열 화면 출력
+		System.out.println(s); // 정수 s 값 화면 출력
 	}
 
 }
