05cd212ac2a1efda979bd5d24aea05c3a2434c00
SPAUPA <sinibida@gmail.com>
2024-09-23 14:47:07 +0900

    +: scanner

--- Ch02_01/src/Car.java
+++ Ch02_01/src/Car.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Car {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
+		String name = scanner.next(); // String 단어 받기
+		
+		System.out.println(name);
+		
+		scanner.close();
 	}
 
 }
