4dda792d8ae6c874b8810b1126bdfee1e1324ae0
SPAUPA <sinibida@gmail.com>
2024-09-23 14:31:37 +0900

    +: circle (as class

--- Ch02_01/src/Car.java
+++ Ch02_01/src/Car.java
@@ -1,13 +1,22 @@
 
+class Circle {
+	public static final double PI = 3.141592;
+	public static double getArea(double radius) {
+		return radius * radius * PI;
+	}
+	public static double getLength(double radius) {
+		return 2 * radius * PI;
+	}
+}
+
 public class Car {
 
 	public static void main(String[] args) {
-		final double PI = 3.141592;
 		
 		double radius = 10.0;
 		
-		double area = radius * radius * PI;
-		double circum = 2 * radius * PI;
+		double area = Circle.getArea(radius);
+		double circum = Circle.getLength(radius);
 		
 		System.out.println("원의 면적 = " + area);
 		System.out.println("원의 둘레 = " + circum);
