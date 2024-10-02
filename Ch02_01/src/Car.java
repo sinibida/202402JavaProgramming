17534045f5f4a859d5ec157a524828e2f09ed76a
SPAUPA <sinibida@gmail.com>
2024-10-02 09:08:19 +0900

    +: 0925

--- Ch02_01/src/Car.java
+++ Ch02_01/src/Car.java
@@ -1,20 +1,14 @@
 import java.util.Scanner;
 
 public class Car {
-	public static int add(int a, int b) {
-		return a + b;
-	}
-	
 	public static void main(String[] args) {
-		int a, b;
-		var scanner = new Scanner(System.in);
-
-		a = scanner.nextInt();
-		b = scanner.nextInt();
-		
-		System.out.println(add(a, b));
-		
-		scanner.close();
+		Scanner in = new Scanner(System.in);
+		System.out.print("수를 입력하시오: ");
+		int number = in.nextInt();
+		if (number % 5 == 0)
+			System.out.println("5의 배수입니다.");
+		else
+			System.out.println("5의 배수가 아닙니다.");
+		in.close();
 	}
-
 }
